1661ef0a0c1d60a8302f8e2cdd29a48fb936ec41
deeep08 <deeep0810@gmail.com>
2020-06-24 17:08:15 +0100

    Added server time in hello message

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = `Hello Deep\nYou are on Cloud at instance: ${hostname()}\n`;
+const message = `Hello Deep\nYou are on Cloud at instance: ${hostname()}\nServer time: ${new Date()} \n`;
 const port = 8080;
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
