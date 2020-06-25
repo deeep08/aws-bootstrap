c538973c180fd540f924ef5dccf691f897896259
deeep08 <deeep0810@gmail.com>
2020-06-25 15:30:45 +0100

    Corrected SED command to fetch region

--- start-service.sh
+++ start-service.sh
@@ -3,7 +3,7 @@ source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
 
 # Query the EC2 metadata service for this instance's region
-REGION="$(wget -qO- http://instance-data/latest/meta-data/placement/availability-zone | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:')"
+REGION="$(wget -qO- http://instance-data/latest/meta-data/placement/availability-zone | sed -r 's:([0-9][0-9]*)[a-z]*$:\1:')"
 # Query the EC2 metadata service for this instance's instance-id
 export INSTANCE_ID="$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)"
 # Query EC2 describeTags method and pull our the CFN Logical ID for this instance
